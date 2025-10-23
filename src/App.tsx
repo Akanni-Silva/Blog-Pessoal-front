701128dbd0230d7abac8d650128451c11650ff09
Akanni Silva <akanni029@gmail.com>
2025-10-23 15:05:25 -0300

    implemantação da navbar e footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import Footer from "./components/footer/footer";
+import Navbar from "./components/navbar/navbar";
 import Home from "./pages/home/home";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+      <Navbar />
+      <Home />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
